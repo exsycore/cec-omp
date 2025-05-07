b2bf83c6177f7f443a747646aaa96392a8d948c7
EasyCore <131441651+exsycore@users.noreply.github.com>
2025-05-08 01:42:04 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,137 +1,106 @@
-#include "sdk.hpp"
+#include "main.hpp"
 
 #include "Server/Components/Pawn/pawn.hpp"
 
-#include "natives.h"
-
-AMX_NATIVE_INFO natives[] =
-	{
-		{"CE_Convert", Natives::CE_Convert},
-		{"CE_Convert_Dialog", Natives::CE_Convert_Dialog},
-		{"CE_CountTag", Natives::CE_CountTag},
-		{"CE_CountVowel", Natives::CE_CountVowel},
-		{0, 0}};
+#include "common.hpp"
+#include "natives.hpp"
 
 class ColourFix final : public IComponent, public CoreEventHandler, public PawnEventHandler
 {
-private:
-	ICore *core_{};
+        PROVIDE_UID(0xBC4C07566F9769D9);
 
-	IPawnComponent *pawn_{};
+        StringView componentName() const override
+        {
+            return "open.mp colourfix";
+        }
 
-public:
-	PROVIDE_UID(0x55D38CB65D394195);
+        SemanticVersion componentVersion() const override
+        {
+            return SemanticVersion(2, 6, 0, 2);
+        }
 
-	StringView componentName() const override
-	{
-		return "open.mp colourfix";
-	}
+        void onLoad(ICore *c) override
+        {
+            core_ = c;
+            c->printLn("      --------------------------------");
+            c->printLn(" ");
+            c->printLn("    < cec 2.6 (open.mp) | Copyright 2020-2025 > ");
+            c->printLn("     Author: Ak-kawit \"B-Less\" Tahae");
+            c->printLn("     Editor: EasyCore \"E-Core\" .....");
+            c->printLn("   Repository: https://github.com/Brian-Less/cec");
+            c->printLn("   Repository: https://github.com/exsycore/cec-omp");
+            c->printLn(" ");
+            c->printLn("      --------------------------------");
+        }
 
-	SemanticVersion componentVersion() const override
-	{
-		return SemanticVersion(2, 6, 0, 2);
-	}
+        void onInit(IComponentList *components) override
+        {
+            ompgdk::GDKManager::Get()->Init(core_, components);
+            pawn_ = components->queryComponent<IPawnComponent>();
 
-	void onLoad(ICore *c) override
-	{
-		core_ = c;
-		getCore() = c;
-		get() = this;
-		core_->printLn("      --------------------------------");
-		core_->printLn(" ");
-		core_->printLn("    < cec 2.6 (open.mp) | Copyright 2020-2025 > ");
-		core_->printLn("     Author: Ak-kawit \"B-Less\" Tahae");
-		core_->printLn("     Editor: EasyCore \"E-Core\" .....");
-		core_->printLn("   Repository: https://github.com/Brian-Less/cec");
-		core_->printLn("   Repository: https://github.com/exsycore/cec-omp");
-		core_->printLn(" ");
-		core_->printLn("      --------------------------------");
-	}
+            if (pawn_ == nullptr)
+            {
+                StringView name = componentName();
+                core_->logLn(
+                    LogLevel::Error,
+                    "Error loading component %.*s: Pawn component not loaded",
+                    name.length(),
+                    name.data());
 
-	void onInit(IComponentList *components) override
-	{
-		pawn_ = components->queryComponent<IPawnComponent>();
+                return;
+            }
 
-		if (pawn_ == nullptr)
-		{
-			StringView name = componentName();
-			core_->logLn(
-				LogLevel::Error,
-				"Error loading component %.*s: Pawn component not loaded",
-				name.length(),
-				name.data());
+            pawn_->getEventDispatcher().addEventHandler(this);
+            core_->getEventDispatcher().addEventHandler(this);
+            pAMXFunctions = (void*)&pawn_->getAmxFunctions();
+        }
 
-			return;
-		}
+        void onReady() override {}
 
-		pawn_->getEventDispatcher().addEventHandler(this);
-		core_->getEventDispatcher().addEventHandler(this);
-		pAMXFunctions = (void *)&pawn_->getAmxFunctions();
-	}
+        void onTick(Microseconds elapsed, TimePoint now) override {}
 
-	void onReady() override {}
+        void onAmxLoad(IPawnScript &script) override
+        {
+            script.Register("CE_Convert", Natives::CE_Convert);
+            script.Register("CE_Convert_Dialog", Natives::CE_Convert_Dialog);
+            script.Register("CE_CountTag", Natives::CE_CountTag);
+            script.Register("CE_CountVowel", Natives::CE_CountVowel);
+            // amx_Register(script.GetAMX(), native_list, -1);
+        }
 
-	void onTick(Microseconds elapsed, TimePoint now) override {}
+        void onAmxUnload(IPawnScript &script) override {}
 
-	void onAmxLoad(IPawnScript &script) override
-	{
-		AMX *amx = script.GetAMX();
+        void onFree(IComponent *component) override
+        {
+            if (component == pawn_ || component == this)
+            {
+                if (pawn_)
+                {
+                    core_->printLn("\n\n*** ColourFix Plugin v2.6 unloaded ***\n\n");
 
-		amx_Register(amx, natives, -1);
-	}
+                    core_->getEventDispatcher().removeEventHandler(this);
+                    pawn_->getEventDispatcher().removeEventHandler(this);
+                    pAMXFunctions = nullptr;
+                }
 
-	void onAmxUnload(IPawnScript &script) override {}
+                pawn_ = nullptr;
+            }
+        }
 
-	void onFree(IComponent *component) override
-	{
-		if (component == pawn_ || component == this)
-		{
-			if (pawn_)
-			{
-				core_->printLn("\n\n*** ColourFix Plugin v2.6 unloaded ***\n\n");
-				
-				core_->getEventDispatcher().removeEventHandler(this);
-				pawn_->getEventDispatcher().removeEventHandler(this);
-			}
+        void free() override
+        {
+            delete this;
+        }
 
-			pawn_ = nullptr;
-			pAMXFunctions = nullptr;
-		}
-	}
+        void reset() override {}
 
-	void free() override
-	{
-		if (pawn_ != nullptr)
-		{
-			pawn_->getEventDispatcher().removeEventHandler(this);
-			core_->getEventDispatcher().removeEventHandler(this);
-		}
+        ~ColourFix() {}
 
-		delete this;
-	}
+    private:
+        ICore *core_ = nullptr;
 
-	void reset() override {}
-
-	~ColourFix() {}
-
-	static ICore *&getCore();
-
-	static ColourFix *&get();
+        IPawnComponent *pawn_ = nullptr;
 };
-
-ICore *&ColourFix::getCore()
-{
-	static ICore *core{};
-
-	return core;
-}
-
-ColourFix *&ColourFix::get()
-{
-	static ColourFix *component{};
-
-	return component;
-}
 
 COMPONENT_ENTRY_POINT()
 {
