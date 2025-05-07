b8b6d16557d348da544a8e1a4953191561eb69fc
EasyCore <131441651+exsycore@users.noreply.github.com>
2025-05-07 17:43:06 +0700

    Fix Unload Plugin

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,19 @@
 #include "natives.h"
 
 AMX_NATIVE_INFO natives[] =
-{
-	{ "CE_Convert", Natives::CE_Convert },
-	{ "CE_Convert_Dialog", Natives::CE_Convert_Dialog },
-	{ "CE_CountTag", Natives::CE_CountTag },
-	{ "CE_CountVowel", Natives::CE_CountVowel },
-	{ 0, 0 }
-};
+	{
+		{"CE_Convert", Natives::CE_Convert},
+		{"CE_Convert_Dialog", Natives::CE_Convert_Dialog},
+		{"CE_CountTag", Natives::CE_CountTag},
+		{"CE_CountVowel", Natives::CE_CountVowel},
+		{0, 0}};
 
 class ColourFix final : public IComponent, public CoreEventHandler, public PawnEventHandler
 {
 private:
-	ICore *core_ = nullptr;
+	ICore *core_{};
 
-	IPawnComponent *pawn_ = nullptr;
+	IPawnComponent *pawn_{};
 
 public:
 	PROVIDE_UID(0x55D38CB65D394195);
@@ -33,9 +32,11 @@ public:
 		return SemanticVersion(2, 6, 0, 2);
 	}
 
-	void onLoad(ICore* c) override
+	void onLoad(ICore *c) override
 	{
 		core_ = c;
+		getCore() = c;
+		get() = this;
 		core_->printLn("      --------------------------------");
 		core_->printLn(" ");
 		core_->printLn("    < cec 2.6 (open.mp) | Copyright 2020-2025 > ");
@@ -58,44 +59,43 @@ public:
 				LogLevel::Error,
 				"Error loading component %.*s: Pawn component not loaded",
 				name.length(),
-				name.data()
-			);
+				name.data());
 
 			return;
 		}
 
 		pawn_->getEventDispatcher().addEventHandler(this);
 		core_->getEventDispatcher().addEventHandler(this);
-		pAMXFunctions = (void*)&pawn_->getAmxFunctions();
+		pAMXFunctions = (void *)&pawn_->getAmxFunctions();
 	}
 
-	void onReady() override
-	{
-	}
+	void onReady() override {}
 
-	void onTick(Microseconds elapsed, TimePoint now) override
-	{
-	}
+	void onTick(Microseconds elapsed, TimePoint now) override {}
 
 	void onAmxLoad(IPawnScript &script) override
-	{	
-		AMX* amx = script.GetAMX();
-		
+	{
+		AMX *amx = script.GetAMX();
+
 		amx_Register(amx, natives, -1);
 	}
 
-	void onAmxUnload(IPawnScript &script) override
-	{
-	}
+	void onAmxUnload(IPawnScript &script) override {}
 
 	void onFree(IComponent *component) override
 	{
-		if (component == pawn_)
+		if (component == pawn_ || component == this)
 		{
+			if (pawn_)
+			{
+				core_->printLn("\n\n*** ColourFix Plugin v2.6 unloaded ***\n\n");
+				
+				core_->getEventDispatcher().removeEventHandler(this);
+				pawn_->getEventDispatcher().removeEventHandler(this);
+			}
 
 			pawn_ = nullptr;
 			pAMXFunctions = nullptr;
-			core_->printLn("\n\n*** ColourFix Plugin v2.6 unloaded ***\n\n");
 		}
 	}
 
@@ -110,14 +110,28 @@ public:
 		delete this;
 	}
 
-	void reset() override
-	{
-	}
+	void reset() override {}
 
-	~ColourFix()
-	{
-	}
+	~ColourFix() {}
+
+	static ICore *&getCore();
+
+	static ColourFix *&get();
 };
+
+ICore *&ColourFix::getCore()
+{
+	static ICore *core{};
+
+	return core;
+}
+
+ColourFix *&ColourFix::get()
+{
+	static ColourFix *component{};
+
+	return component;
+}
 
 COMPONENT_ENTRY_POINT()
 {
